df40eefaf115e86c16277402346699b5cc6d1386
smartdev212 <smartdev212@gmail.com>
2019-03-18 18:39:27 -0400

    get the chat sessions

--- src/app/chat/components/chat-message/chat-message.component.ts
+++ src/app/chat/components/chat-message/chat-message.component.ts
@@ -11,6 +11,7 @@ import {ChatMessage} from '../../models/chatMessage';
 })
 export class ChatMessageComponent implements OnInit {
   messages = 'Begin Chat';
+  chatSession: ChatSession;
   message: string;
 
   constructor(@Inject(MAT_DIALOG_DATA)
@@ -20,32 +21,43 @@ export class ChatMessageComponent implements OnInit {
               },
               public dialogRef: MatDialogRef<any>,
               private chatService: ChatService) {
+    this.chatSession = data.chatSession;
   }
 
   get sendAllowed() {
     return this.message && this.message.length > 0;
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.pollChatSession();
+  }
+
+  pollChatSession() {
+    setTimeout(async () => {
+      const newChatSess = await this.chatService.getChatSessionById(this.chatSession.id);
+      console.log('got new Chat Session messages', newChatSess.messages);
+      this.pollChatSession();
+
+    }, 5000);
   }
 
   async onSend() {
 
-    const chatSession: ChatSession = this.data.chatSession;
     const chatMessages: ChatMessage[] = [
       {
         id: 0,
-        sender: this.data.isAccountManager ? chatSession.chatResponderName :
-          chatSession.chatInitiatorName,
+        sender: this.data.isAccountManager ? this.chatSession.chatResponderName :
+          this.chatSession.chatInitiatorName,
         message: this.message
       }
     ];
     if (this.message && this.message.length > 0) {
+      const sendResult = await this.chatService.sendChatMessages(chatMessages, this.chatSession.id);
+      console.log('send result', sendResult);
+
       this.messages += '\n' + this.message;
       this.message = '';
     }
-    const sendResult  = await this.chatService.sendChatMessages(chatMessages, chatSession.id);
-    console.log('send result', sendResult);
   }
 
 }
